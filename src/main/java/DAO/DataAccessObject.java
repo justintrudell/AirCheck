7c765f2e25e8b8d11a4dd9fa7486798e2baac320
Vishal Kuo <vfkuo@uwaterloo.ca>
2016-04-23 03:53:37 -0400

    UTF=8

--- src/main/java/DAO/DataAccessObject.java
+++ src/main/java/DAO/DataAccessObject.java
@@ -29,7 +29,7 @@ public class DataAccessObject {
             jobj.addProperty("sneezing", rs.getString("sneezing"));
             jobj.addProperty("nose_block", rs.getBoolean("nose_block"));
             jobj.addProperty("itchy_eyes", rs.getBoolean("itchy_eyes"));
-            jobj.addProperty("city", rs.getString("city"));≠
+            jobj.addProperty("city", rs.getString("city"));
             jarr.add(jobj);
         }
         rs.close();
