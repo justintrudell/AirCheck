86e9ab2cc9aa7222e6327d38bb1ad862fa504003
Justin Trudell <justin.trudell.4@gmail.com>
2016-04-23 03:43:33 -0400

    JS

--- src/main/resources/public/test.js
+++ src/main/resources/public/test.js
@@ -14,6 +14,8 @@ window.onload = function(){
         function displayLocation(position) {
           var latitude = Math.round(position.coords.latitude);
           var longitude = Math.round(position.coords.longitude);
+          document.getElementById('latitude').value = latitude;
+          document.getElementById('longitude').value = longitude;
           var request = new XMLHttpRequest();
 
           var method = 'GET';
