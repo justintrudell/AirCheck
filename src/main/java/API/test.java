a26f9972d9003e3b2623350beef25f4bc1852a48
Vishal Kuo <vfkuo@uwaterloo.ca>
2016-04-22 20:54:26 -0400

    Added api

---
+++ src/main/java/API/test.java
@@ -0,0 +1,29 @@
+package API;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+/**
+ * Created by vishalkuo on 2016-04-22.
+ */
+public class test {
+    static OkHttpClient client = new OkHttpClient();
+    public static void main(String[] args){
+        try{
+            String test = run("http://api.openweathermap.org/pollution/v1/co/0.0,10.0/2016-03-01Z.json?appid=2e5380c813086bfa4cff625b78e8996b");
+            System.out.println(test);
+        } catch (Exception e){
+            return;
+        }
+
+    }
+
+    static String run(String url) throws IOException{
+        Request request = new Request.Builder().url(url).build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+}
