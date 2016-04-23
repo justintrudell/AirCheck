10a613772e0f8e9d9c1dc8895e5d4d12c5387179
Justin Trudell <justin.trudell.4@gmail.com>
2016-04-22 22:58:42 -0400

    postin stuff

--- src/main/java/API/Entry.java
+++ src/main/java/API/Entry.java
@@ -7,14 +7,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
+
 /**
  * Created by vishalkuo on 2016-04-22.
  */
 public class Entry {
     public static void main(String[] args){
         Map<String, String> map = new HashMap<>();
-        map.put("message", "Hello, World!");
+        map.put("message", "");
 
         get("/", (req, res) -> new ModelAndView(map, "hello"), new JadeTemplateEngine());
+
+
+        get("/data", (req, res) -> {
+            res.redirect("/");
+            map.replace("message", "hey");
+        return null;});
     }
 }
