72527e0a72793c9ef51a03b6725322bc552decde
Vishal Kuo <vfkuo@uwaterloo.ca>
2016-04-23 02:16:57 -0400

    Create table

--- src/main/java/API/test.java
+++ src/main/java/API/test.java
@@ -2,6 +2,7 @@ package API;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 /**
  * Created by vishalkuo on 2016-04-22.
@@ -9,9 +10,24 @@ import java.sql.DriverManager;
 public class test {
    public static void main(String args[]){
        Connection c = null;
+       Statement stmt = null;
        try {
            Class.forName("org.sqlite.JDBC");
            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+
+           stmt = c.createStatement();
+           String sql = "CREATE TABLE Users(" +
+                   "cough_level int, " +
+                   "breath int, " +
+                   "wheezing int, " +
+                   "sneezing int, " +
+                   "nose_block boolean, " +
+                   "itchy_eyes boolean, " +
+                   "latitude real, " +
+                   "longitude real)";
+           stmt.executeUpdate(sql);
+           stmt.close();
+           c.close();
        } catch ( Exception e ) {
            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
            System.exit(0);
