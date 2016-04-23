ba7cf67448e3e77df4c885cdce8bfe362420f46e
Jerry Wang <yisenjerrywang@gmail.com>
2016-04-23 18:58:22 -0400

    Typeform

--- src/main/java/Helpers/AirCheckConstants.java
+++ src/main/java/Helpers/AirCheckConstants.java
@@ -6,6 +6,8 @@ package Helpers;
 public class AirCheckConstants {
     public static final String ApiBaseUrl = "http://api.openweathermap.org";
     public static final String ApiToken = "2e5380c813086bfa4cff625b78e8996b";
+    public static final String TypeformApi = "3c664b74af9b1aadf9d563ead04dd82c7932623e";
+    public static final String TypeformUid = "UlrIrF";
     public static final String MonoxidePrecision = "precision";
     public static final String MonoxidePressure = "pressure";
     public static final String MonoxideValue = "value";
