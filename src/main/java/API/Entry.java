6e427ff75df5500b3dd88a60a67329433d73d190
Vishal Kuo <vfkuo@uwaterloo.ca>
2016-04-22 22:27:02 -0400

    ADd getmonoxide

--- src/main/java/API/Entry.java
+++ src/main/java/API/Entry.java
@@ -1,5 +1,6 @@
 package API;
 
+import Models.Monoxide;
 import spark.ModelAndView;
 import spark.template.jade.JadeTemplateEngine;
 
@@ -7,6 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+
 /**
  * Created by vishalkuo on 2016-04-22.
  */
@@ -16,5 +18,19 @@ public class Entry {
         map.put("message", "Hello, World!");
 
         get("/", (req, res) -> new ModelAndView(map, "hello"), new JadeTemplateEngine());
+        testMonoxide();
+
+
     }
+
+    public static void testMonoxide(){
+        try{
+            Monoxide m = GetMonoxide.GetMonoxide(0.0, 10.0);
+            System.out.println(m.getPrecision());
+        } catch(Exception e){
+            return;
+        }
+
+    }
+
 }
